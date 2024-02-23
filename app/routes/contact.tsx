57f7f64dd525e7a885cd478d4fe94192a7177099
Urias-Flores <93843706+Urias-Flores@users.noreply.github.com>
2024-02-23 00:43:04 -0600

    update

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -18,7 +18,7 @@ export default function contact (){
   const [message, setMessage] = useState('')
 
   return (
-    <main className='container'>
+    <main>
       <h1 className="heading">
         Contáctanos
       </h1>
@@ -26,25 +26,27 @@ export default function contact (){
       <h3 className='subheading'>
         Para tus consultas llena el siguiente formulario:
       </h3>
+      
+      <div className="container">
+        <img src="/images/about-banner.jpg" alt="banner" className='banner_image'/>
 
-      <img src="/images/about-banner.jpg" alt="banner" className='banner_image'/>
+        <form className='form' action="post">
+          <input type="text" placeholder='Nombre' value={name} onChange={(e) => { setName( e.target.value ) }}/>
+          <input type="text" placeholder='Correo Electroncio' value={email} onChange={(e) => { setEmail(e.target.value) }}/>
 
-      <form className='form' action="post">
-        <input type="text" placeholder='Nombre' value={name} onChange={(e) => { setName( e.target.value ) }}/>
-        <input type="text" placeholder='Correo Electroncio' value={email} onChange={(e) => { setEmail(e.target.value) }}/>
+          <input type="text" placeholder='Teléfono' value={phone} onChange={(e) => { setPhone(e.target.value) }}/>
+          <textarea
+            name="message"
+            id="message"
+            placeholder='Escribe tu mensaje aqui'
+            value={message}
+            onChange={(e) => { setMessage(e.target.value) }}
+          >
+          </textarea>
 
-        <input type="text" placeholder='Teléfono' value={phone} onChange={(e) => { setPhone(e.target.value) }}/>
-        <textarea
-          name="message"
-          id="message"
-          placeholder='Escribe tu mensaje aqui'
-          value={message}
-          onChange={(e) => { setMessage(e.target.value) }}
-        >
-        </textarea>
-
-        <input type="submit" value='Enviar'/>
-      </form>
+          <input type="submit" value='Enviar'/>
+        </form>
+      </div>
     </main>
   )
 }
