0ef991710e6019d59a5b151f2e5bb56cef6364df
Urias-Flores <93843706+Urias-Flores@users.noreply.github.com>
2024-02-23 18:15:27 -0600

    adding scrollable

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
-import { Link } from "@remix-run/react";
+import { Link, redirect } from "@remix-run/react";
+import { useState } from "react";
 import stylesMain from '~/styles/main.css';
 export function links(){
   return [
@@ -23,40 +24,102 @@ export function meta(){
 }
 
 export default function Index() {
+  const [memberShowed, setMemberShowed] = useState(1);
+
+
   return (
     <main>
       <div className='container'>
         <h1 className='heading'>Equipo grupo sosa morales</h1>
 
-        <section className='team'>
-          <div className='member'>
-            <img src="/images/Marco_Sosa.webp" alt="menber" className='member_photo'/>
-            <h3 className='member_name'>Marco Tulio Sosa Morales</h3>
-            <p className='member_information'>Abogado y notario publico</p>
-            <p className='member_information'>Director Grupo Sosa Morales</p>
-          </div>
+        <section className='group'>
+          <div className="team_container">
+            <Link to={`#member${ memberShowed - 1 }`} className={`${ memberShowed === 1 && 'hide'}`}>
+              <img 
+                src="/images/chevron-left.svg" 
+                alt="chevron" 
+                className="team_chevron"
+                onClick={ () => { setMemberShowed(memberShowed - 1) }}
+              />
+            </Link>
 
-          <div className='member'>
-            <img src="/images/Javier_Barahona.webp" alt="menber" className='member_photo'/>
-            <h3 className='member_name'>Abog. Javier B. Viera</h3>
-            <p className='member_information'>Bufete Barahona Viera</p>
-            <p className='member_information'>La Ceiba, Atlantida, Honduras.</p>
-          </div>
+            <div className="team">
+              <div className='member' id="member1">
+                <img src="/images/Marco_Sosa.webp" alt="menber" className='member_photo'/>
+                <div className="member_content">
+                  <h3 className='member_name'>Marco Tulio Sosa Morales</h3>
+                  <p className='member_information'>Abogado y notario publico</p>
+                  <p className='member_information'>Director Grupo Sosa Morales</p>
+                </div>
+              </div>
 
-          <div className='member'>
-            <img src="/images/Vivian_Sevilla.webp" alt="menber" className='member_photo'/>
-            <h3 className='member_name'>Abog. Vivian M. Sevilla</h3>
-            <p className='member_information'>Firma Legal Sevilla</p>
-            <p className='member_information'>Tela, Atlantida, Honduras</p>
-          </div>
+              <div className='member' id="member2">
+                <img src="/images/Javier_Barahona.webp" alt="menber" className='member_photo'/>
+                <div className="member_content">
+                  <h3 className='member_name'>Abog. Javier B. Viera</h3>
+                  <p className='member_information'>Bufete Barahona Viera</p>
+                  <p className='member_information'>La Ceiba, Atlantida, Honduras.</p>
+                </div>
+              </div>
 
-          <div className='member'>
-            <img src="/images/Luis_Chavez.webp" alt="menber" className='member_photo'/>
-            <h3 className='member_name'>Abog. Luis A. Chavez</h3>
-            <p className='member_information'>Bufete Soluciones Juridicas</p>
-            <p className='member_information'>Tela, Atlantida, Honduras</p>
+              <div className='member' id="member3">
+                <img src="/images/Vivian_Sevilla.webp" alt="menber" className='member_photo'/>
+                <div className="member_content">
+                  <h3 className='member_name'>Abog. Vivian M. Sevilla</h3>
+                  <p className='member_information'>Firma Legal Sevilla</p>
+                  <p className='member_information'>Tela, Atlantida, Honduras</p>
+                </div>
+              </div>
+
+              <div className='member' id="member4">
+                <img src="/images/Luis_Chavez.webp" alt="menber" className='member_photo'/>
+                <div className="member_content">
+                  <h3 className='member_name'>Abog. Luis A. Chavez</h3>
+                  <p className='member_information'>Bufete Soluciones Juridicas</p>
+                  <p className='member_information'>Tela, Atlantida, Honduras</p>
+                </div>
+              </div>
+            </div>
+
+            <Link to={`#member${ memberShowed + 1 }`}  className={`${ memberShowed === 4 && 'hide'}`}>
+              <img 
+                src="/images/chevron-rigth.svg" 
+                alt="chevron" 
+                className="team_chevron"
+                onClick={ () => { setMemberShowed(memberShowed + 1) }}
+              />
+            </Link>
+          </div>
+          
+          <div className="selected">
+            <Link 
+              className={`selected_point ${memberShowed === 1 && 'active'}`} 
+              to='#member1' 
+              onClick={() => { setMemberShowed(1) }}
+              >
+            </Link>
+            <Link 
+              className={`selected_point ${memberShowed === 2 && 'active'}`} 
+              to='#member2' 
+              onClick={() => { setMemberShowed(2) }}
+              >
+            </Link>
+            <Link 
+              className={`selected_point ${memberShowed === 3 && 'active'}`} 
+              to='#member3' 
+              onClick={() => { setMemberShowed(3) }}
+              >
+            </Link>
+            <Link 
+              className={`selected_point ${memberShowed === 4 && 'active'}`} 
+              to='#member4' 
+              onClick={() => { setMemberShowed(4) }}
+              >
+            </Link>
           </div>
         </section>
+
+        
 
         <h2 className='heading'>Servicios que ofrece Grupo Sosa Morales</h2>
 
