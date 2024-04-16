64bb2e7c436882cfa6edba640cfb5d1c9155e2a6
Urias-Flores <floresmeza808@gmail.com>
2024-04-16 16:31:30 -0600

    Adding map on page contact

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -55,10 +55,25 @@ export default function contact (){
             onChange={(e) => { setMessage(e.target.value) }}
           >
           </textarea>
-
           <input type="submit" value='Enviar'/>
         </form>
       </div>
+
+      <section className="address">
+          <h1 className="heading">
+              Encuentranos aqui
+            </h1>
+
+          <iframe 
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3839.4239911765508!2d-87.44862560596671!3d15.781582061195305!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f687de6aa1465d1%3A0xbf9ba1a64076d3f1!2sGRUPO%20SOSA%20MORALES%20%26%20ASOCIADOS!5e0!3m2!1ses-419!2shn!4v1713305575485!5m2!1ses-419!2shn" 
+          width="100%" 
+          height="450"
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+          className="map"
+          >
+          </iframe>
+        </section>
     </main>
   )
 }
